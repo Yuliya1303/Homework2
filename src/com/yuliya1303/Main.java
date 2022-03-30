7264798d419170fc7b1a3b5f372ab10374853eca
Yuliya Byshko <Yuliyalevkovets1303@gmail.com>
2022-03-30 17:19:39 +0300

    Added 3 line

--- src/com/yuliya1303/Main.java
+++ src/com/yuliya1303/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("The first line by Yuliya");
         System.out.println("The second line by Yuliya");
+        System.out.println("The 3 line by Yuliya");
     }
 }
