d4e4c8bdcaa25d15f2e714e279278087ff050a14
YuliyaB <Yuliyalevkovets1303@gmail.com>
2022-03-30 17:15:43 +0300

    Added new method
    Added new method by Valery to test git merge

--- src/com/yuliya1303/Main.java
+++ src/com/yuliya1303/Main.java
@@ -6,4 +6,8 @@ public class Main {
         System.out.println("The first line by Yuliya");
         System.out.println("The second line by Yuliya");
     }
+    
+    public static void main(String[] args) {
+        System.out.println("The first line by Valery");
+    }
 }
